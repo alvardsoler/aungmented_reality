0aeac004636cb4e6999e6447cadd42680a4453a5
alvardsoler <alsoler@ucm.es>
2015-11-14 13:06:00 +0100

    añadido primer proyecto de prueba

---
+++ projects/virtualButtons/Assets/OnDragRobot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnDragRobot : MonoBehaviour, DragControl.OnDragListener {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void onDrag (float horiontal, float vertical)
+	{
+		gameObject.transform.Rotate(new Vector3(horiontal,vertical,0));
+	}
+
+}
