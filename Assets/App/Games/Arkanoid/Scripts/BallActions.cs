a353d1fdb45056e74248f9a8404cd68e851f74d7
raul <racobos@ucm.es>
2016-01-14 23:44:02 +0100

    New structured project created and added the doc folder with the base memory document

---
+++ Assets/App/Games/Arkanoid/Scripts/BallActions.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallActions : MonoBehaviour {
+
+	public bool start = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (!start && Input.touchCount > 0) {
+			start = true;
+			GetComponent<Rigidbody> ().AddForce(new Vector3(Input.touches[0].position.x * 0.1f,Input.touches[0].position.y * 0.1f,0f));
+		}
+
+
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.tag == "blocks")
+		{
+			Destroy(col.gameObject);
+		}
+	}
+}
